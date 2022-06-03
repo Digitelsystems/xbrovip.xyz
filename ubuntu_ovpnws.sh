4417d1c8d1e37dfe25af33b630137f372780e861
Digitelsystems <103522787+Digitelsystems@users.noreply.github.com>
2022-06-03 03:01:55 -0700

    Update ubuntu_ovpnws.sh

--- ubuntu_ovpnws.sh
+++ ubuntu_ovpnws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='173.82.65.242';
-USER='xbrovip_xbrovip';
-PASS='dig21#0550013763';
-DBNAME='xbrovip_xbrovip';
+HOST='66.45.248.110';
+USER='digitelp_xbrovip';
+PASS='ME2HE2Di@';
+DBNAME='digitelp_xbrovip';
 
 install_require()
 {
